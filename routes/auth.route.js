ad2d84d8e48708be1021896af2ae6a12be099f29
Oluwafemi Erota <erota.oluwafemi@gmail.com>
2023-01-08 23:00:36 +0100

    Update auth.route.js
    added comment

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,9 +3,16 @@ const express = require('express')
 const authRouter = express.Router()
 const passport = require('passport')
 
-
+// POST /auth/signup
+// Sign up a new user with the provided username and password
+// Request body: { username: String, password: String }
+// Response: 201 if successful, 400 if invalid request data or username already exists
 authRouter.post('/signup', passport.authenticate('signup', { session: false }), controller.signup)
 
+// POST /auth/login
+// Log in an existing user with the provided username and password
+// Request body: { username: String, password: String }
+// Response: 200 if successful, 400 if invalid request data or username doesn't exist, 401 if invalid password
 authRouter.post('/login', controller.login)
 
 module.exports = authRouter;
