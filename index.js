8c88d702ee6fe8e58d2b054f5747dbae984b115a
Oluwafemi Erota <erota.oluwafemi@gmail.com>
2023-01-09 00:16:02 +0100

    Update index.js
    add comment

--- index.js
+++ index.js
@@ -7,23 +7,27 @@ const port = process.env.PORT || 3000;
 const passport = require('passport')
 const authRouter = require('./routes/auth.route')
 
-
+// Middleware to handle passport authentication
 require('./middleware/passport')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
+// Import routes
 const { blogRouter } = require('./routes/blog.route')
 const { route } = require('./routes/blog.route')
 
+// Use the imported routes and apply passport authentication
 app.use('/user', passport.authenticate('jwt', { session: false }), blogRouter)
 app.use('/', authRouter)
 app.use('/', route)
 
+// Endpoint to test if the server is running correctly
 app.get("/homepage", (req, res) => {
     return res.status(200).json({ message: "successful" })
 })
 
+// Default route for all other requests
 app.use("*", (req, res) => {
     return res.status(404).json({ message: "route not found" })
 })
