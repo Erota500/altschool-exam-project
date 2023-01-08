aacdb9f8821664f54b4f271377ca0df5861f4d22
Oluwafemi Erota <erota.oluwafemi@gmail.com>
2023-01-08 23:10:45 +0100

    Update database.js
    added comment

--- database/database.js
+++ database/database.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 require('dotenv').config({ path: 'config.env' })
 
-
+// Connect to the MongoDB database specified in the 'MONGO_URL' environment variable
+// The connection is established when the 'connected' event is emitted
+// If there is an error while connecting, the 'error' event is emitted with the error object as an argument
 function connectMongoDB() {
     mongoose.connect(process.env.MONGO_URL)
     mongoose.connection.on('connected', () => {
